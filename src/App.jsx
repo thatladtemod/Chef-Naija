944c57320abae9ef00a75b8aa40cac11edf951fa
Omoniyi Temitayo <temit3684@gmail.com>
2025-07-11 13:12:45 +0100

    feat: Add Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Header from "./Header"
 import Hero from "./Hero"
+import Footer from "./Footer"
 
 export default function App() {
 
   return (
     <>
       <Header />
-      <Hero />
+      <main>
+        <Hero />
+      </main>
+      <Footer />
     </>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+
+    return (
+      <footer>
+        <div className="rule"></div>
+        <p className="footer-message">
+          &copy; {new Date().getFullYear} Chef Naija. Made with ❤️ for Nigerian food lovers
+          worldwide.
+        </p>
+      </footer>
+    );
+}
