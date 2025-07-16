7cb3cf74e7154ab7cd1a8a2fd5c6e8db06929cf7
Omoniyi Temitayo <temit3684@gmail.com>
2025-07-16 02:22:31 +0100

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -14,13 +14,16 @@ export default function App() {
     setLoading(true)
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/api/recipe", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ food: delicacy }),
-      });
+      const response = await fetch(
+        `${import.meta.env.CHEF_NAIJA_BACKEND}/api/recipe`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ food: delicacy }),
+        }
+      );
 
       console.log(response)
 
